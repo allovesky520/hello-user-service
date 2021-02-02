52179119d582f7fb4cdaab44de70c500d01dfc45
yyzh <allovesky@126.com>
2021-02-02 16:09:30 +0800

    Update hello-tools/src/main/java/com/springboot/demo/domin/hello/service/impl/HelloServiceImpl.java

---
+++ hello-tools/src/main/java/com/springboot/demo/domin/hello/service/impl/HelloServiceImpl.java
@@ -0,0 +1,19 @@
+package com.springboot.demo.domin.hello.service.impl;
+
+import com.springboot.demo.domin.hello.service.HelloService;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Service
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String hello(String str){
+        return "Hello Spring Boot";
+    }
+
+    public void niubi(){
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+    }
+}
