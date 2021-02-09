d6eedfc16f457be611a9e6b3b8c87c6ad2577fc4
hanning <allovesky@126.com>
2021-02-09 17:03:57 +0800

    Update hello-tools/src/main/java/com/springboot/demo/domin/tools/service/impl/VerifyCodeServiceImpl.java
    redis、mysql驱动、druid连接池

--- hello-tools/src/main/java/com/springboot/demo/domin/tools/service/impl/VerifyCodeServiceImpl.java
+++ hello-tools/src/main/java/com/springboot/demo/domin/tools/service/impl/VerifyCodeServiceImpl.java
@@ -1,23 +1,71 @@
 package com.springboot.demo.domin.tools.service.impl;
 
-import cn.hutool.captcha.LineCaptcha;
+import com.springboot.demo.constant.PasswordConstant;
 import com.springboot.demo.domin.tools.service.VerifyCodeService;
-import com.springboot.demo.util.ValidateCodeImpUtil;
+import com.springboot.demo.util.Base64Utils;
+import com.springboot.demo.util.Md5Util;
+import com.springboot.demo.util.ValidateCodeUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 public class VerifyCodeServiceImpl implements VerifyCodeService {
 
+    @Autowired
+    @Qualifier(value = "ssoRedisTemplate")
+    private RedisTemplate redisTemplate;
+
+    @Value(value = "#{T(java.lang.Integer).parseInt('${sso.forgot_verify_code_effect_mins:5}')}")
+    private Integer verifyCodeEffectMins;
+
+    private final String prefix = "verify_code_";
+
     @Override
-    public void getVerifyCode() {
+    public void getVerifyCode(String phone) {
 //        LineCaptcha lineCaptcha = new LineCaptcha(200, 100, 4, 150);
-//        ValidateCodeImpUtil.buildImageRes(lineCaptcha.getImageBytes());
+//        ValidateCodeUtil.buildImageRes(lineCaptcha.getImageBytes());
 
-        ValidateCodeImpUtil validatecode = new ValidateCodeImpUtil(80,26,40,4);
-        System.out.println(validatecode.getCode());
-        ValidateCodeImpUtil.buildImageRes(validatecode.getBuffImgBytes());
+        ValidateCodeUtil validateCode = new ValidateCodeUtil(80,26,40,4);
+        String encode = Md5Util.encryptMD5(Base64Utils.encode(validateCode.getCode().toUpperCase()));
+        //  获取请求头中内容，比如浏览器信息
+        HttpServletRequest request = null;
+        request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        //  可以考虑加盐增加复杂度防破解
+        String uuidCode = Md5Util.encryptMD5(Base64Utils.encode(request.getHeader("User-Agent").toUpperCase()));
+        //  拼接key
+        String key = getVerifyKey(phone, uuidCode, PasswordConstant.VERIFY_CODE_TYPE_REGISTER);
 
+        //将生成的随机码存储到Redis中
+        redisTemplate.opsForValue().set(key, encode, verifyCodeEffectMins, TimeUnit.MINUTES);
+        log.info("redis验证码{}",redisTemplate.opsForValue().get(key));
+        ValidateCodeUtil.buildImageRes(validateCode.getBuffImgBytes());
+
+    }
+
+    /**
+     *
+     * @param phone
+     * @param userAgent
+     * @param verifyType
+     * @return
+     */
+    private String getVerifyKey(String phone, String userAgent, String verifyType) {
+        final StringBuffer buffer = new StringBuffer(this.prefix);
+        buffer.append(phone)
+                .append("_")
+                .append(userAgent)
+                .append("_")
+                .append(verifyType);
+        return buffer.toString();
     }
 }
