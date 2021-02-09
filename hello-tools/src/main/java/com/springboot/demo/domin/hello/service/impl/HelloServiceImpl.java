48af6ab8c77cd49084538f379926fe3bcf2875df
hanning <allovesky@126.com>
2021-02-09 17:03:40 +0800

    Update hello-tools/src/main/java/com/springboot/demo/domin/hello/service/impl/HelloServiceImpl.java
    redis、mysql驱动、druid连接池

--- hello-tools/src/main/java/com/springboot/demo/domin/hello/service/impl/HelloServiceImpl.java
+++ hello-tools/src/main/java/com/springboot/demo/domin/hello/service/impl/HelloServiceImpl.java
@@ -1,16 +1,37 @@
 package com.springboot.demo.domin.hello.service.impl;
 
 import com.springboot.demo.domin.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class HelloServiceImpl implements HelloService {
+
+    @Autowired
+    @Qualifier(value = "ssoRedisTemplate")
+    private RedisTemplate redisTemplate;
+
+    @Value(value = "#{T(java.lang.Integer).parseInt('${sso.forgot_verify_code_effect_mins:5}')}")
+    private Integer verifyCodeEffectMins;
+
     @Override
     public String hello(String str){
         return "Hello Spring Boot";
+    }
+
+    @Override
+    public String testRedis(String str) {
+
+        //将生成的随机码存储到Redis中
+        redisTemplate.opsForValue().set(str, "abc", verifyCodeEffectMins, TimeUnit.MINUTES);
+        return redisTemplate.opsForValue().get(str).toString();
     }
 
     public void niubi(){
